4958ce82483c2b3cd44a6538d16b4337b8377e32
Sterling <sterlingdiazd@gmail.com>
2022-06-12 13:12:59 -0400

    ngStyle

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -4,31 +4,39 @@ import { Component } from '@angular/core';
 	templateUrl: './server.component.html'
 })
 export class ServerComponent {
-    allowNewServer = false
+	allowNewServer = false
 	serverName = ''
 	serverCreated = false
 	serverCreationStatus = 'No server was created'
+	serverId: number = 10
+	serverStatus: string = 'offline'
 
 	/**
 	 *
 	 */
 	constructor() {
+		this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline'
 		setTimeout(() => {
 			this.allowNewServer = true
 		}, 2000)
-
 	}
 	onCreateServer() {
 		if (this.serverName.length == 0) {
 			this.serverCreated = false
 		} else {
-
-		this.serverCreated = true
-		this.serverCreationStatus = 'Server was created! Name is '
+			this.serverCreated = true
+			this.serverCreationStatus = 'Server was created! Name is '
 		}
 	}
 	onUpdateServerName(event: Event) {
 		this.serverName = (<HTMLInputElement>event.target).value
-		
+	}
+
+	getServerStatus() {
+		return this.serverStatus
+	}
+
+	getColor() {
+		return this.serverStatus === 'online' ? 'green' : 'red'
 	}
 }
