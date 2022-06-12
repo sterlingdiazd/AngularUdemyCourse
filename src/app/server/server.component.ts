9671dadaf79299ef58d14b2dda8bc21c5113ad4d
Sterling <sterlingdiazd@gmail.com>
2022-06-12 12:57:40 -0400

    ngIf

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,9 +1,34 @@
 import { Component } from '@angular/core';
 @Component({
-    selector: 'app-server',
-    templateUrl: './server.component.html'
+	selector: 'app-server',
+	templateUrl: './server.component.html'
 })
-
 export class ServerComponent {
+    allowNewServer = false
+	serverName = ''
+	serverCreated = false
+	serverCreationStatus = 'No server was created'
 
+	/**
+	 *
+	 */
+	constructor() {
+		setTimeout(() => {
+			this.allowNewServer = true
+		}, 2000)
+
+	}
+	onCreateServer() {
+		if (this.serverName.length == 0) {
+			this.serverCreated = false
+		} else {
+
+		this.serverCreated = true
+		this.serverCreationStatus = 'Server was created! Name is '
+		}
+	}
+	onUpdateServerName(event: Event) {
+		this.serverName = (<HTMLInputElement>event.target).value
+		
+	}
 }
